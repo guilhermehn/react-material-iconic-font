e1faa8cb8456c0d507df55687abb442a57bf047d
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2016-03-23 21:50:18 -0300

    Moving build task to webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  entry: './src/index.js',
+
+  target: 'node',
+
+  resolve: {
+    extensions: ['', '.js']
+  },
+
+  output: {
+    path: './dist',
+    filename: 'index.js',
+    library: 'MaterialIcon',
+    libraryTarget: 'umd'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  externals: [
+    nodeExternals()
+  ]
+}
