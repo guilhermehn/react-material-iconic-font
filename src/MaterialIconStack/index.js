0d6abb7814d4bca53cad011db6ab95df1115c0cb
Guilherme Nagatomo <gnagatomo@uolinc.com>
2018-11-06 15:24:28 -0200

    Prettier + folder structure

--- src/MaterialIconStack/index.js
+++ src/MaterialIconStack/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 const MaterialIconStack = ({ children, large }) => {
-  let cx = {
+  const cx = {
     'zmdi-hc-stack': true,
     'zmdi-hc-lg': large
   };
