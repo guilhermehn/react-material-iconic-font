8249a95fc8c05c52f8c329fe19918d167bf11916
Guilherme Nagatomo <gnagatomo@uolinc.com>
2019-11-08 14:12:59 -0300

    Fixed tests

--- __tests__/MaterialIcon.js
+++ __tests__/MaterialIcon.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import MaterialIcon from '../src';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -7,128 +7,123 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('MaterialIcon tests', () => {
 	it('should be a <i> tag with `zmdi` className', () => {
-		expect(shallow(<MaterialIcon type="plus" />).is('i.zmdi')).toBe(true);
+		expect(mount(<MaterialIcon type="plus" />).find('i.zmdi')).toHaveLength(1);
 	});
 
-	it('should add the class `zmdi-hc-lg` if the `large` property is present', () => {
-		expect(shallow(<MaterialIcon type="plus" large />).is('.zmdi-hc-lg')).toBe(
-			true
-		);
-	});
-
-	it('should add the class `zmdi-hc-{n}x` if the `size` property is present', () => {
-		let size = 2;
-
+	it('should add classname `zmdi-hc-lg` if the `large` property is present', () => {
 		expect(
-			shallow(<MaterialIcon type="plus" size={size} />).is(`.zmdi-hc-${size}x`)
-		).toBe(true);
+			mount(<MaterialIcon type="plus" large />).find('.zmdi-hc-lg')
+		).toHaveLength(1);
 	});
 
-	it('should not add the class `zmdi-hc-{n}x` if the `size` property is invalid (1 > n < 6)', () => {
-		let size = 6;
-
+	it('should add classname `zmdi-hc-{n}x` if the `size` property is present', () => {
 		expect(
-			shallow(<MaterialIcon type="plus" size={size} />).is(`.zmdi-hc-${size}x`)
-		).toBe(false);
+			mount(<MaterialIcon type="plus" size={2} />).find(`.zmdi-hc-2x`)
+		).toHaveLength(1);
 	});
 
-	it('should add the class `zmdi-hc-fw` if the `fixed` property is present', () => {
-		expect(shallow(<MaterialIcon type="plus" fixed />).is('.zmdi-hc-fw')).toBe(
-			true
-		);
-	});
-
-	it('should add the class `zmdi-hc-li` if the `list` property is present', () => {
-		expect(shallow(<MaterialIcon type="plus" list />).is('.zmdi-hc-li')).toBe(
-			true
-		);
-	});
-
-	it('should add the class `zmdi-hc-border` if the `border` property is present', () => {
+	it('should not add classname `zmdi-hc-{n}x` if the `size` property is invalid (1 > n < 6)', () => {
 		expect(
-			shallow(<MaterialIcon type="plus" border />).is('.zmdi-hc-border')
-		).toBe(true);
+			mount(<MaterialIcon type="plus" size={6} />).find(`.zmdi-hc-6x`)
+		).toHaveLength(0);
 	});
 
-	it('should add the class `zmdi-hc-border-circle` if the `borderCircle` property is present', () => {
+	it('should add classname `zmdi-hc-fw` if the `fixed` property is present', () => {
 		expect(
-			shallow(<MaterialIcon type="plus" borderCircle />).is(
+			mount(<MaterialIcon type="plus" fixed />).find('.zmdi-hc-fw')
+		).toHaveLength(1);
+	});
+
+	it('should add classname `zmdi-hc-li` if the `list` property is present', () => {
+		expect(
+			mount(<MaterialIcon type="plus" list />).find('.zmdi-hc-li')
+		).toHaveLength(1);
+	});
+
+	it('should add classname `zmdi-hc-border` if the `border` property is present', () => {
+		expect(
+			mount(<MaterialIcon type="plus" border />).find('.zmdi-hc-border')
+		).toHaveLength(1);
+	});
+
+	it('should add classname `zmdi-hc-border-circle` if the `borderCircle` property is present', () => {
+		expect(
+			mount(<MaterialIcon type="plus" borderCircle />).find(
 				'.zmdi-hc-border-circle'
 			)
-		).toBe(true);
+		).toHaveLength(1);
 	});
 
-	it('should add the class `zmdi-hc-spin` if the `spin` property is present', () => {
-		expect(shallow(<MaterialIcon type="plus" spin />).is('.zmdi-hc-spin')).toBe(
-			true
-		);
-	});
-
-	it('should add the class `zmdi-hc-spin-reverse` if the `spin` and `reverse` properties are present', () => {
+	it('should add classname `zmdi-hc-spin` if the `spin` property is present', () => {
 		expect(
-			shallow(<MaterialIcon type="plus" spin reverse />).is(
+			mount(<MaterialIcon type="plus" spin />).find('.zmdi-hc-spin')
+		).toHaveLength(1);
+	});
+
+	it('should add classname `zmdi-hc-spin-reverse` if the `spin` and `reverse` properties are present', () => {
+		expect(
+			mount(<MaterialIcon type="plus" spin reverse />).find(
 				'.zmdi-hc-spin-reverse'
 			)
-		).toBe(true);
+		).toHaveLength(1);
 	});
 
-	it('should add the class `pull-left` if the `pull` property is `left`', () => {
+	it('should add classname `pull-left` if the `pull` property is `left`', () => {
 		expect(
-			shallow(<MaterialIcon type="plus" pull="left" />).is('.pull-left')
-		).toBe(true);
+			mount(<MaterialIcon type="plus" pull="left" />).find('.pull-left')
+		).toHaveLength(1);
 	});
 
-	it('should add the class `pull-right` if the `pull` property is `right`', () => {
+	it('should add classname `pull-right` if the `pull` property is `right`', () => {
 		expect(
-			shallow(<MaterialIcon type="plus" pull="right" />).is('.pull-right')
-		).toBe(true);
+			mount(<MaterialIcon type="plus" pull="right" />).find('.pull-right')
+		).toHaveLength(1);
 	});
 
-	it('should add the class `zmdi-hc-flip-horizontal` if the `flip` property is `horizontal`', () => {
+	it('should add classname `zmdi-hc-flip-horizontal` if the `flip` property is `horizontal`', () => {
 		expect(
-			shallow(<MaterialIcon type="plus" flip="horizontal" />).is(
+			mount(<MaterialIcon type="plus" flip="horizontal" />).find(
 				'.zmdi-hc-flip-horizontal'
 			)
-		).toBe(true);
+		).toHaveLength(1);
 	});
 
-	it('should add the class `zmdi-hc-flip-vertical` if the `flip` property is `vertical`', () => {
+	it('should add classname `zmdi-hc-flip-vertical` if the `flip` property is `vertical`', () => {
 		expect(
-			shallow(<MaterialIcon type="plus" flip="vertical" />).is(
+			mount(<MaterialIcon type="plus" flip="vertical" />).find(
 				'.zmdi-hc-flip-vertical'
 			)
-		).toBe(true);
+		).toHaveLength(1);
 	});
 
-	it('should not add the class `zmdi-hc-flip-{axis}` if the `flip` value is other than `horizontal` or `vertical`', () => {
-		let icon = <MaterialIcon type="plus" flip="foo" />;
+	it('should not add classname `zmdi-hc-flip-{axis}` if the `flip` value is other than `horizontal` or `vertical`', () => {
+		const icon = <MaterialIcon type="plus" flip="foo" />;
 
-		expect(shallow(icon).is('.zmdi-hc-flip-horizontal')).toBe(false);
-
-		expect(shallow(icon).is('.zmdi-hc-flip-vertical')).toBe(false);
+		expect(mount(icon).find('.zmdi-hc-flip-horizontal')).toHaveLength(0);
+		expect(mount(icon).find('.zmdi-hc-flip-vertical')).toHaveLength(0);
 	});
 
-	it('should add the class `zmdi-hc-rotate-{degrees}` if the `rotate` property is one of the valid rotation values', () => {
+	it('should add classname `zmdi-hc-rotate-{degrees}` if the `rotate` property is one of the valid rotation values', () => {
 		expect(
-			shallow(<MaterialIcon type="plus" rotate={90} />).is('.zmdi-hc-rotate-90')
-		).toBe(true);
+			mount(<MaterialIcon type="plus" rotate={90} />).find('.zmdi-hc-rotate-90')
+		).toHaveLength(1);
 
 		expect(
-			shallow(<MaterialIcon type="plus" rotate={180} />).is(
+			mount(<MaterialIcon type="plus" rotate={180} />).find(
 				'.zmdi-hc-rotate-180'
 			)
-		).toBe(true);
+		).toHaveLength(1);
 
 		expect(
-			shallow(<MaterialIcon type="plus" rotate={270} />).is(
+			mount(<MaterialIcon type="plus" rotate={270} />).find(
 				'.zmdi-hc-rotate-270'
 			)
-		).toBe(true);
+		).toHaveLength(1);
 	});
 
-	it('should not add the class `zmdi-hc-rotate-{degrees}` if the `degrees` value is other than `90`, `180` or `270`', () => {
-		let icon = <MaterialIcon type="plus" rotate={60} />;
+	it('should not add classname `zmdi-hc-rotate-{degrees}` if the `degrees` value is other than `90`, `180` or `270`', () => {
+		const icon = <MaterialIcon type="plus" rotate={60} />;
 
-		expect(shallow(icon).is('.zmdi-hc-rotate-60')).toBe(false);
+		expect(mount(icon).is('.zmdi-hc-rotate-60')).toBe(false);
 	});
 });
